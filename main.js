397a53c920dab734d2fee9451fc656ace7130a80
nasubi <smiley.jinro2525@gmail.com>
2025-11-28 23:34:51 +0900

    教材P.363 Firebase セットアップまで

---
+++ main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBjGz8AP_IBQZIaWm-5w0ZJQcXIsceGlQ",
+  authDomain: "daily-report-e2bdd.firebaseapp.com",
+  projectId: "daily-report-e2bdd",
+  storageBucket: "daily-report-e2bdd.firebasestorage.app",
+  messagingSenderId: "1082825651206",
+  appId: "1:1082825651206:web:1c3f1aff0e277f7390bf87"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
