d2b5b8a56e91e79ebb397ea01f78ef441913d70e
nasubi <smiley.jinro2525@gmail.com>
2025-11-30 01:00:16 +0900

    ソースコード比較

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
-import { fetchHistoryData } from "./my-modules/fetchHistoryData";
+import { fetchHistoryData } from "./my-modules/fetch-history-data";
 import { submitData } from "./my-modules/submit-data";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_AROJECT_ID,
+  projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.APP_ID
@@ -27,5 +27,5 @@ if (document.getElementById("js-history")) {
 
 // ページ内にid"js-form"が存在すれば非同期の関数を実行
 if(document.getElementById("js-form")) {
-  document.getElementById("js-form").addEventListener("submit", (e) =>  submitData(e, addDoc, collection, db));
+  document.getElementById("js-form").addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
 };
