04d611e9adc92830647e53c1561e43934317a91f
nasubi <smiley.jinro2525@gmail.com>
2025-11-29 20:14:33 +0900

    教材P.376まで　データ送信・保存

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -29,18 +29,37 @@ const fetchHistoryData = async () => {
 
   querySnapshot.forEach((doc) => {
     console.log(`${doc.id} => ${doc.data()}`);
-    tags += 
-    `<tr><td>${doc.data().date}</td>
-    <td>${doc.data().name}</td>
-    <td>${doc.data().work}</td>
-    <td>${doc.data().comment}</td></tr>`
+    tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>`;
   });
 
   document.getElementById("js-history").innerHTML = tags;
 };
 
-
 // ページ内にid"js-history"が存在すれば非同期の関数を実行
-if(document.getElementById("js-history")){
-  fetchHistoryData();
+if (document.getElementById("js-history")) {
+  fetchHistoryData(getDocs, collection, db);
+}
+
+// 非同期の関数 Cloud Firestoreにデータを送信
+const submitData = async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  try {
+    const docRef = await addDoc(collection(db, "reports"), {
+      date: new Date(),
+      name: formData.get("name"),
+      work: formData.get("work"),
+      comment: formData.get("comment"),
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+// ページ内にid"js-form"が存在すれば非同期の関数を実行
+if(document.getElementById("js-form")) {
+  document.getElementById("js-form").addEventListener("submit"), (e) =>  submitData(e, addDoc, collection, db);
 }
